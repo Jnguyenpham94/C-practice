c580526c586a2c16b02e50cc6316579ee16fd953
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-23 21:21:22 -0700

    average using Average()

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+//Write a C# Sharp program that takes as many numbers as input to calculate and print the average.
+Console.WriteLine("Enter a string of numbers separated by commas for their average: ");
+
+string? input = Console.ReadLine();
+if (input == null)
+{
+    Console.WriteLine("unexpected number. FAIL");
+}
+else
+{
+    string[] subs = input.Split(",");
+
+    List<int> result = new List<int>(Array.ConvertAll(subs, int.Parse));
+    Console.WriteLine(result.Average());
+    Console.WriteLine(result.Sum());
+}
