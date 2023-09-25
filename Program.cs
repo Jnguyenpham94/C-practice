ef021207735fbf7eff5ccaf953a11233a9abbbc2
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-09-24 22:06:18 -0700

    reverse words & palindrome DONE
    substring in string START

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ class MyClass
             //menu options
             Console.WriteLine("1. Average");
             Console.WriteLine("2. Reverse String");
-            Console.WriteLine("2. Reverse word order");
+            Console.WriteLine("3. Reverse word order");
+            Console.WriteLine("4. Reverse word order with join");
+            Console.WriteLine("5. Palindrome");
             Console.WriteLine("exit");
             string? input = Console.ReadLine();
             if (input == null)
@@ -35,6 +37,16 @@ class MyClass
                 case "3":
                     {
                         ReverseWordOrder();
+                        break;
+                    }
+                case "4":
+                    {
+                        ReverseWordOrderJoin();
+                        break;
+                    }
+                case "5":
+                    {
+                        IsPalindrome();
                         break;
                     }
                 case "exit":
@@ -104,9 +116,60 @@ class MyClass
         }
         else
         {
-
+            string[] subs = input.Split();
+            string result = "";
+            for (int i = subs.Length - (1); i >= 0; i--)
+            {
+                result += subs[i] + " ";
+            }
+            Console.WriteLine(result);
         }
 
+    }
+
+    public static void ReverseWordOrderJoin()
+    {
+        Console.Write("Enter a sentence to reverse: ");
+        string? input = Console.ReadLine();
+        if (input == null)
+        {
+            Console.WriteLine("unexpected number. FAIL");
+        }
+        else
+        {
+            string[] subs = input.Split();
+            Array.Reverse(subs);
+            string result = String.Join(" ", subs);
+            Console.WriteLine(result);
+        }
+
+    }
+
+    public static void IsPalindrome()
+    {
+        Console.Write("Enter a word to check if it is a palindrome: ");
+        string? input = Console.ReadLine();
+        if (input == null)
+        {
+            Console.WriteLine("unexpected number. FAIL");
+        }
+        else
+        {
+            //for loop method
+            string reverse = "";
+            for (int i = input.Length - (1); i >= 0; i--)
+            {
+                reverse += input[i];
+            }
+            if (input == reverse)
+            {
+                Console.WriteLine($"{input} is a palindrome!");
+            }
+            else 
+            {
+                Console.WriteLine($"{input} is NOT a palindrome!");
+            }
+        }
     }
 }
 
